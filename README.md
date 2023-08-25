5a1da3f898e660625e4002460727d62ab7ed1581
Juliana Carraro <julianacarraroy@gmail.com>
2023-08-25 07:17:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Testes de performance com k6
+
+Projeto final do curso de K6 básico da Qacademy ministrado por [Fernando Papito](https://www.linkedin.com/in/papitoio/) 
+
+## 🔖 Testes realizados
+
+
+* [ ] Teste de performance com K6 
+* [ ] Load testing (teste de carga) 
+* [ ] Smoke testing (teste de fumaça)
+* [ ] Stress testing (teste de estresse)
+
+
+## 🔖 Requisitos não funcionais
+
+### Cadastro
+
+- [ ] O cadastro com sucesso deve ocorrer em até 2 segundos
+- [ ] Cadastros sem sucesso devem ocorrer em até 2 segundos
+- [ ] Deve poder cadastrar até 100 usuários simultâneos
+- [ ] A margem de erro no cadastro deve ser de pelo menos 1%
+
+
+## 🚀 Tecnologias
+
+- [Node.js] - plataforma de desenvolvimento
+- [Express] - framework onde a API foi construída
+- [MongoDB] - Banco de dados (Não relacional)
+- [k6] - ferramenta para teste de carga, performance, stress etc...
+
+## 📝 License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
---
+++ tests/signup-stress.js
@@ -0,0 +1,51 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+
+import uuid from './libs/uuid.js'
+
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
+
+export const options = {
+  stages: [
+    { duration: '2m', target: 100 }, //below normal load
+    { duration: '5m', target: 100 },
+    { duration: '2m', target: 200 }, //normal load
+    { duration: '5m', target: 200 },
+    { duration: '2m', target: 300 }, //around breaking point
+    { duration: '5m', target: 300 },
+    { duration: '2m', target: 400 }, //beyond the breaking point
+    { duration: '5m', target: 400 },
+    { duration: '10m', target: 0 } //scale down. Recovery stage
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<2000'], //95% das requisicoes devem responder em até 2s
+    http_req_failed: ['rate<0.01'] //1% das requisicoes podem ocorrer erro
+  }
+}
+
+export default function () {
+  const url = 'http://localhost:3333/signup'
+
+  const payload = JSON.stringify({ email: `${uuid.v4().substring(24)}@qa.empresa.com.br`, password: 'pwd123' })
+
+  const headers = {
+    'headers': {
+      'Content-Type': 'application/json'
+    }
+  }
+  const res = http.post(url, payload, headers)
+
+  //console.log(res.body)
+
+  check(res, {
+    'status should be 201': (r) => r.status === 201
+  })
+
+  sleep(1);
+}
